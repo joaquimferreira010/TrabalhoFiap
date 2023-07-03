fa979b9707afe8165b03c5e741f9aee948bff288
Joaquim Ferreira Filho <joaquimferreira010@gmail.com>
2023-07-03 08:20:25 -0300

    Primeiro Versão
    Primeiro Versão

---
+++ FarmaciaApp/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using FarmaciaApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FarmaciaApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        private static List<ProdutoViewModel> produtos = new List<ProdutoViewModel>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Produto()
+        {
+           
+            return View(produtos);
+        }
+
+        public IActionResult Create()
+        {
+            var produtoVm = new ProdutoViewModel();
+            return View(produtoVm);
+        }
+        public IActionResult CreateProduto(ProdutoViewModel produtoViewModel)
+        {
+            //return View("Produto");
+            produtos.Add(produtoViewModel);
+            return RedirectToAction(nameof(Produto));
+
+        }
+
+        public IActionResult Projeto()
+        {
+            return View();
+        }
+       
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FarmaciaApp/Models/ProdutoViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FarmaciaApp.Models
+{
+    public class ProdutoViewModel
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+    }
+}
